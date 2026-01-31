a0de1e64533684c435b53f781eee36f2141f41a0
Ca088 <pranaylovesis@gmail.com>
2026-01-31 18:43:58 +0530

    Add class1 for string input and display

--- act.py
+++ act.py
@@ -1,1 +1,13 @@
 
+class class1:
+    def __init__(self):
+        self.x=""
+    def Enter(self):
+        self.x=input("Enter a string:")
+     
+    def display(self):
+        print("You entered:",self.x.upper())
+
+obj1=class1()
+obj1.Enter()
+obj1.display()
